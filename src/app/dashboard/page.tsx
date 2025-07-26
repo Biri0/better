df86d02b7e8548c0a23a8fe4fcb4fe968d9459b8
Mariano Riefolo <rfmariano@protonmail.com>
2025-07-26 11:06:42 +0200

    Improve dashboard interface and display mockup data in table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,15 @@
 import { auth } from "~/server/auth";
 import { redirect } from "next/navigation";
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from "~/components/ui/table";
 
 export default async function Dashboard() {
   const session = await auth();
@@ -8,10 +18,43 @@ export default async function Dashboard() {
     redirect("/api/auth/signin?callbackUrl=/dashboard");
   }
 
+  const bets = [
+    { title: "My bet", createdBy: "Me", expires: "Tomorrow" },
+    { title: "Another bet", createdBy: "Alice", expires: "Next week" },
+    { title: "Yet another bet", createdBy: "Bob", expires: "Next month" },
+  ];
+
   return (
-    <h1>
-      Hello, {session.user.name ?? "World"}! You have{" "}
-      {session.user.credits ?? 0} credits!
-    </h1>
+    <div className="mx-32 mt-4">
+      <Card>
+        <CardHeader>Hi {session.user.name}!</CardHeader>
+        <CardContent>You have {session.user.credits} credits</CardContent>
+      </Card>
+      <div className="my-16 flex justify-center">
+        <Button>Create a bet</Button>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead>Created by</TableHead>
+            <TableHead>Expires</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {bets.map((bet, idx) => (
+            <TableRow key={idx}>
+              <TableCell>{bet.title}</TableCell>
+              <TableCell>{bet.createdBy}</TableCell>
+              <TableCell>{bet.expires}</TableCell>
+              <TableCell>
+                <Button>Show more</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
