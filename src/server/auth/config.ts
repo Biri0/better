c427088dd96bbc09cee44afc7d94d9dff21bc138
Mariano Riefolo <rfmariano@protonmail.com>
2025-07-27 21:08:41 +0200

    Add Google Auth provider and env variables

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 import {
@@ -41,6 +42,7 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     DiscordProvider,
+    GoogleProvider,
     /**
      * ...add more providers here.
      *
